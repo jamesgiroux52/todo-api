3ce07e93676440a5b3aa4cb673a21221c9d9d410
JamesMac <james@jgiroux.com>
2017-04-10 20:57:34 -0400

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.post('/todos', (req, res) => {
     todo.save().then((doc) => {
         res.send(doc);
     }, (e) => {
-        res.send(e);
+        res.status(400).send(e);
     });
 });
 
@@ -24,3 +24,5 @@ app.post('/todos', (req, res) => {
 app.listen(3000, () => {
     console.log('Server started on port 3000');
 });
+
+module.exports = {app};
